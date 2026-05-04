efa3061d87371a476a4971697cb65baf9833595f
Kupas Ozul <kupasozul@gmail.com>
2026-05-05 00:59:22 +0200

    Seed data hozzáadása

--- LibrarySystem.Api/Migrations/20260504225837_SeedData.Designer.cs
+++ LibrarySystem.Api/Migrations/20260504225837_SeedData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LibrarySystem.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibrarySystem.Api.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260504225837_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.7");
